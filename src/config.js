758951db4d626b993a16cac5188a1d8652fb2ec7
Ying Liu <yingl1984@gmail.com>
2021-03-26 14:30:19 -0700

    deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 export default {
   "folderapi":  "https://noteful-server-101.herokuapp.com/api/folders",
   "noteapi": "https://noteful-server-101.herokuapp.com/api/notes"
+  // "folderapi":  "http://localhost:8000/api/folders",
+  // "noteapi": "http://localhost:8000/api/notes"
 }
 
